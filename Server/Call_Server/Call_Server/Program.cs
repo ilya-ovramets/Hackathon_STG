fa230c586ce31c2643134b5c51e7b3f64a2029e8
Elay <ovramecilla@gmail.com>
2023-10-21 17:25:55 +0300

    Create Server

---
+++ Server/Call_Server/Call_Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+class VoiceChatServer
+{
+    private TcpListener server;
+    private List<ClientHandler> clients;
+
+    
+
+    public VoiceChatServer()
+    {
+        clients = new List<ClientHandler>();
+        server = new TcpListener(IPAddress.Any, 8000);
+        server.Start();
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            ClientHandler handler = new ClientHandler(client);
+
+            clients.Add(handler);
+            handler.Run();
+        }
+    }
+
+    
+}
+
+class ClientHandler
+{
+    public ClientHandler(TcpClient client)
+    {
+        this.client = client;
+    }
+
+    private TcpClient client;
+    // rest of handling code
+
+    public void Run()
+    {
+        while (true)
+        {
+            // 1. Receive audio
+            // 2. Relay audio to other clients
+        }
+    }
+}
